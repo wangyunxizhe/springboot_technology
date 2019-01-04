bd8dd1845114773886ff7d2853f3d615b6feca94
wangyunxizhe <543456229@qq.com>
2019-01-04 17:41:14 +0800

    springboot入门示例--初步搭建

---
+++ introduction/src/main/java/com/yuan/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.yuan.controller;
+
+import com.yuan.dao.UserDao;
+import com.yuan.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private final UserDao userDao;
+
+    @Autowired
+    public UserController(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @PostMapping("/person/save")
+    public User save(@RequestParam String name) {
+        User user = new User();
+        user.setName(name);
+        if (userDao.save(user)) {
+            System.out.printf("用户对象：% 保存成功！\n", user);
+        }
+        return user;
+    }
+}
---
+++ introduction/src/main/java/com/yuan/dao/UserDao.java
@@ -0,0 +1,32 @@
+package com.yuan.dao;
+
+import com.yuan.domain.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Repository
+public class UserDao {
+
+    //采用内存型的存储方式-->Map
+    private final ConcurrentMap<Integer, User> repo = new ConcurrentHashMap<>();
+
+    //做自增id用
+    private final static AtomicInteger idGenerator = new AtomicInteger();
+
+    /**
+     * 标准javadoc写法：保存用户对象
+     *
+     * @param user {@link User}    用户对象
+     * @return 如果保存成功，返回<code>true</code>；否则，返回<code>false</code>
+     */
+    public boolean save(User user) {
+        //id从1开始
+        Integer id = idGenerator.incrementAndGet();
+        user.setId(id);
+        return repo.put(id, user) == null;
+    }
+
+}
